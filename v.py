e69acb6af80874fc671c10f63b7b052e0a20187f
Mateusz Czajka <mateusz.czajka@netguru.pl>
2013-12-12 17:51:49 +0100

    add ratio

--- v.py
+++ v.py
@@ -9,8 +9,15 @@ from os import listdir
 
 files_path = './train/'
 files = listdir(files_path)
+detected = 0
 
 for f_name in files:
   original_gender = f_name[4]
+  deteceted_gender = 'K'
+
   wave.open(files_path + f_name, 'r')
-  print original_gender
+
+  if original_gender == deteceted_gender:
+    detected += 1
+
+print float(detected)/float(len(files))
