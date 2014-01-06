616457675cd16e7a043941939169251b8a1a046a
Mateusz Czajka <mateusz.czajka@netguru.pl>
2014-01-06 15:40:20 +0100

    preprocess file path

--- v.py
+++ v.py
@@ -13,47 +13,30 @@ import os
 import wave
 from scikits.audiolab import wavread
 
-def freq_from_crossings(sig, fs):
-  indices = find((sig[1:] >= 0) & (sig[:-1] < 0))
-  crossings = [i - sig[i] / (sig[i+1] - sig[i]) for i in indices]
-  return fs / mean(diff(crossings))
-
-def freq_from_autocorr(sig, fs):
-  corr = fftconvolve(sig, sig[::-1], mode='full')
-  corr = corr[len(corr)/2:]
-
-  d = diff(corr)
-  start = find(d > 0)[0]
-
-  px = argmax(corr[start:]) + start
-
-  return fs / px
-
-def m_frequency(path):
-
+def file_preprocessing(path):
   data, fs, encoding = wavread(path)
   signal = [mean(d) for d in data]
 
-  wfile = wave.open(path, "r")
-  T = (1.0 * wfile.getnframes()) / wfile.getframerate()
+  f = wave.open(path, "r")
+  frames = f.getnframes()
+  fs = f.getframerate()
+  f.close()
 
-  wfile.close()
+  return (signal, fs, frames)
 
-  sec = 2
+def m_frequency(data):
 
-  if sec != None:
-    if T > sec: #one second is enought
-      T = sec
-  n = T * fs
-  n = int(n)
+  signal, fs, frames = data
 
-  if sec != None:
-    signal = signal[0:n]
+  T = frames / fs
+  period = 1.5
+  n = int(min(T, period) * fs)
+
+  signal = signal[0:n]
 
   amplitude = abs(fft(signal)) #glosnosc
   frequency = linspace(0, fs, n)
 
-  #sound less then 85 or higher then 255 are unreachable for humans
   amp, freq = [], []
   for i in range(len(frequency)):
     if 85 < frequency[i] < 230:
@@ -86,9 +69,9 @@ count = 0
 for f_name in files:
 
   original_gender = f_name[len(f_name) - 5]
-  detected_gender = 'K'
 
-  if m_frequency(f_name) < 175:
+  detected_gender = 'K'
+  if m_frequency(file_preprocessing(f_name)) < 175:
     detected_gender = 'M'
 
   count += 1
