0008e099f0702d6761db3630eeb7dfbd5fb8a8bb
Mateusz Czajka <mateusz.czajka@netguru.pl>
2014-01-06 15:15:32 +0100

    81.6% success

--- v.py
+++ v.py
@@ -1,83 +1,72 @@
 #!/usr/local/bin/python
 # -*- coding: utf-8 -*-
 
+from __future__ import division
+import scipy.io.wavfile
+import struct
 from pylab import *
-import wave
-import numpy as np
-import matplotlib.pyplot as plt
-import scipy as sp
+from numpy import *
+from scipy import *
+from os import listdir
+import sys
 import os
 import wave
+from scikits.audiolab import wavread
 
-K = 3          # number of windows
-L = 8192       # 1st pass window overlap, 50%
-M = 16384      # 1st pass window length
-N = 32768      # 1st pass DFT lenth: acyclic correlation
+def freq_from_crossings(sig, fs):
+  indices = find((sig[1:] >= 0) & (sig[:-1] < 0))
+  crossings = [i - sig[i] / (sig[i+1] - sig[i]) for i in indices]
+  return fs / mean(diff(crossings))
 
-def welch(x, w, L, N):
-    # Welch's method
-  M = len(w)
-  K = (len(x) - L) / (M - L)
-  Xsq = zeros(N/2+1)                  # len(N-point rfft) = N/2+1
-  for k in range(K):
-    m = k * ( M - L)
-    xt = w * x[m:m+M]
-    Xsq = Xsq + abs(rfft(xt, N)) ** 2
-  Xsq = Xsq / K
-  Wsq = abs(rfft(w, N)) ** 2
-  bias = irfft(Wsq)                   # for unbiasing Rxx and Sxx
-  p = dot(x,x) / len(x)               # avg power, used as a check
-  return Xsq, bias, p
+def freq_from_autocorr(sig, fs):
+  corr = fftconvolve(sig, sig[::-1], mode='full')
+  corr = corr[len(corr)/2:]
 
-def detect_frequency(g, fs):
+  d = diff(corr)
+  start = find(d > 0)[0]
 
-  K = 3
-  L = 8192
-  M = 16384
-  N = 32768
+  px = argmax(corr[start:]) + start
 
-  g = g / float64(max(abs(g)))
-  mi = len(g) / 4
+  return fs / px
 
-  x = g[mi:mi + K*M - (K-1)*L]
-  w = hamming(M)
+def m_frequency(path):
 
-  Xsq, bias, p = welch(x, w, L, N)
-  Rxx = irfft(Xsq)
-  Rxx = Rxx / bias
-  mp = argmax(Rxx[28:561]) + 28
+  data, fs, encoding = wavread(path)
+  signal = [mean(d) for d in data]
 
-  N = M = L - (L % mp)
-  x = g[mi:mi+K*M]
-  w = ones(M); L = 0
-  Xsq, bias, p = welch(x, w, L, N)
-  Rxx = irfft(Xsq)
-  Rxx = Rxx / bias
-  mp = argmax(Rxx[28:561]) + 28
+  wfile = wave.open(path, "r")
+  T = (1.0 * wfile.getnframes()) / wfile.getframerate()
 
-  Sxx = Xsq / bias[0]
-  Sxx[1:-1] = 2 * Sxx[1:-1]
-  Sxx = Sxx / N
-  n0 = N / mp
-  np = argmax(Sxx[n0-2:n0+3]) + n0-2
+  wfile.close()
 
-  m = -1
-  m_i = -1
-  s = 0
-  fq = 0
-  for x in range(0, len(Sxx)):
-    if x > 14 and x < 45:
-      s = s + Sxx[x]
-      fq = fq + x * Sxx[x]
+  sec = 2
 
-  return fq / s * 5.5
+  if sec != None:
+    if T > sec: #one second is enought
+      T = sec
+  n = T * fs
+  n = int(n)
 
-  subplot2grid((2,1), (1,0))
-  title('Power Spectral Density, S$_{xx}$'); xlabel('Frequency (Hz)')
-  fr = r_[:5 * np]; f = fs * fr / N;
-  vlines(f, 0, Sxx[fr], colors='b', linewidth=2)
-  grid(); axis('tight'); ylim(0,1.25*max(Sxx[fr]))
-  show()
+  if sec != None:
+    signal = signal[0:n]
+
+  amplitude = abs(fft(signal)) #glosnosc
+  frequency = linspace(0, fs, n)
+
+  #sound less then 85 or higher then 255 are unreachable for humans
+  amp, freq = [], []
+  for i in range(len(frequency)):
+    if 85 < frequency[i] < 230:
+      freq.append(frequency[i])
+      amp.append(amplitude[i])
+
+  maxAmp = max(amp)
+  for i in range(len(amp)):
+    if amp[i] == maxAmp:
+      freqOfMax = freq[i]
+
+  return freqOfMax
+
 
 def list_files(files):
   processed = []
@@ -92,33 +81,23 @@ files = os.listdir(files_path)
 files = list_files(files)
 
 detected = 0
-avg_f = 0.0
+count = 0
 
 for f_name in files:
 
   original_gender = f_name[len(f_name) - 5]
-  deteceted_gender = 'K'
+  detected_gender = 'K'
 
-  f = wave.open(f_name)
+  if m_frequency(f_name) < 175:
+    detected_gender = 'M'
 
-  fs = f.getframerate()
-  signal = f.readframes(-1)
-  signal = np.fromstring(signal, 'Int16')
-
-  freq = detect_frequency(signal, fs)
-
-  if freq > 154.0:
-    deteceted_gender = 'M'
-
-  avg_f = avg_f + freq
-
-  f.close()
-
-  if original_gender == deteceted_gender:
+  count += 1
+  if original_gender == detected_gender:
     detected += 1
 
+  print detected_gender, " / ", original_gender, '\t', "{0:.0f}%".format(float(detected)/float(count)*100)
+
 print float(detected)/float(len(files))
-print float(avg_f)/float(len(files))
 
 
 
