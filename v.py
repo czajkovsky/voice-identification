51ae6e06853838c3e9a4c403c176da8f52d80ff2
Mateusz Czajka <mateusz.czajka@netguru.pl>
2014-01-07 07:19:59 +0100

    extract classification to method, finish at 83% accuracy

--- v.py
+++ v.py
@@ -13,16 +13,18 @@ import os
 import wave
 from scikits.audiolab import wavread
 
+files_path = './train/'
+
 def line_stats(detected_k, count_k, detected_m, count_m, detected, original_gender):
 
   if count_k == 0:
-    ratio_k = '\t\t-'
+    ratio_k = '\t\tK:-'
   else:
     ratio_k = float(detected_k) / float(count_k) * 100
     ratio_k = "\t\tK:{0:.0f}%".format(ratio_k)
 
   if count_m == 0:
-    ratio_m = '\t\t-'
+    ratio_m = '\t\tM:-'
   else:
     ratio_m = float(detected_m) / float(count_m) * 100
     ratio_m = "\t\tM:{0:.0f}%".format(ratio_m)
@@ -43,12 +45,11 @@ def file_preprocessing(path):
 
   return (signal, fs, frames)
 
-def m_frequency(data):
+def m_frequency(data, period):
 
   signal, fs, frames = data
 
   T = frames / fs
-  period = 1.7
   n = int(min(T, period) * fs)
 
   signal = signal[0:n]
@@ -71,41 +72,52 @@ def list_files(files):
       processed.append(files_path + f)
   return processed
 
-files_path = './train/'
+def classify_gender(period, diff, stats):
 
-files = os.listdir(files_path)
-files = list_files(files)
+  files = os.listdir(files_path)
+  files = list_files(files)
 
-detected_k = 0
-detected_m = 0
-count_k = 0
-count_m = 0
+  detected_k = 0
+  detected_m = 0
+  count_k = 0
+  count_m = 0
 
-for f_name in files:
+  for f_name in files:
 
-  original_gender = f_name[len(f_name) - 5]
+    original_gender = f_name[len(f_name) - 5]
 
-  detected_gender = 'K'
-  detected = False
+    detected_gender = 'K'
+    detected = False
 
-  if m_frequency(file_preprocessing(f_name)) < 171:
-    detected_gender = 'M'
+    if m_frequency(file_preprocessing(f_name), period) < diff:
+      detected_gender = 'M'
 
-  if original_gender == 'K':
-    count_k += 1
-    if detected_gender == original_gender:
-      detected = True
-      detected_k += 1
-  else:
-    count_m += 1
-    if detected_gender == original_gender:
-      detected = True
-      detected_m += 1
+    if original_gender == 'K':
+      count_k += 1
+      if detected_gender == original_gender:
+        detected = True
+        detected_k += 1
+    else:
+      count_m += 1
+      if detected_gender == original_gender:
+        detected = True
+        detected_m += 1
 
-  line_stats(detected_k, count_k, detected_m, count_m, detected, original_gender)
+    if stats:
+      line_stats(detected_k, count_k, detected_m, count_m, detected, original_gender)
 
-print "---------------"
-print "M: ", detected_m, "/", count_m, "({0:.0f}%)".format(float(detected_m) / float(count_m) * 100)
-print "K: ", detected_k, "/", count_k, "({0:.0f}%)".format(float(detected_k) / float(count_k) * 100)
-print "---------------"
-print "G: ", detected_m + detected_k, "/", count_m + count_k, "({0:.0f}%)".format(float(detected_k + detected_m) / float(count_k + count_m) * 100)
+  if stats:
+    print "---------------"
+    print "M: ", detected_m, "/", count_m, "({0:.0f}%)".format(float(detected_m) / float(count_m) * 100)
+    print "K: ", detected_k, "/", count_k, "({0:.0f}%)".format(float(detected_k) / float(count_k) * 100)
+    print "---------------"
+    print "G: ", detected_m + detected_k, "/", count_m + count_k, "({0:.0f}%)".format(float(detected_k + detected_m) / float(count_k + count_m) * 100)
+
+  return (detected_m, detected_k)
+
+if __name__ == '__main__':
+  period = 1.7
+  diff = 170
+  stats = True
+  detected_m, detected_k = classify_gender(period, diff, stats)
+
